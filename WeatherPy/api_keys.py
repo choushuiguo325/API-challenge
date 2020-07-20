5d1b1df093780ba43d9b6c2700eeec8c32685918
Jiaping shen <65934350+choushuiguo325@users.noreply.github.com>
2020-07-20 14:23:21 -0500

    uploaded all the files

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "3290e1c259387f186d4f7cdfdaaed648"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCFedJta6XSuWOnpPNlZxvzfGH4FQNDTkk"
